2550064d3f9453dce04fbb1b963cd15184bcf782
Philip Zubel <philip.zubel1@gmail.com>
2020-08-14 22:59:55 +0200

    Updates

---
+++ precache-manifest.8dabb8cd91570d390d8f17eb76b35f19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a833300b608594c0654bae299774ce2",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "4b9e8162751a417ca5d9",
+    "url": "/covid19tracker/static/css/main.d0d57aee.chunk.css"
+  },
+  {
+    "revision": "80c808a32e961209ce65",
+    "url": "/covid19tracker/static/js/2.caa2a277.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19tracker/static/js/2.caa2a277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9e8162751a417ca5d9",
+    "url": "/covid19tracker/static/js/main.1affb04a.chunk.js"
+  },
+  {
+    "revision": "e9384ca59b4aaebd36e4",
+    "url": "/covid19tracker/static/js/runtime-main.46a5d509.js"
+  }
+]);
